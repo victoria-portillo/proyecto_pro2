32523c853210ae07a613b53af9386c3ec63a5600
santospasman <spasman@udesa.edu.ar>
2023-04-12 14:13:01 -0300

    data semicompleto

---
+++ catologo-productos/data/data.js
@@ -0,0 +1,86 @@
+const moduloDatos = {
+    usuario: {
+      email: 'miguel.estrada@gmail.com',
+      usuario: 'miguelestrada24',
+      contraseña: 'Miguelestrada2001',
+      fechaNacimiento: new Date('2001-03-24'),
+      nroDocumento: 44126876,
+      fotoPerfil: '../public/images/users/default-image.png'
+    },
+    productos: [
+      {
+        nombre: '1984',
+        descripción: 'Esta novela distópica, escrita por George Orwell, imagina un futuro totalitario en el que el gobierno controla todos los aspectos de la vida de las personas, incluso sus pensamientos. "1984" es una crítica feroz al autoritarismo y una advertencia sobre los peligros del poder sin restricciones.',
+        imagen: '../public/images/products/george-orwell-1984-1024x706.jpeg',
+        fechaCarga: 
+      },
+      {
+        nombre: 'El gran Gatsby',
+        descripción: 'Esta novela escrita por F. Scott Fitzgerald, esta ambientada en la década de 1920 sigue la vida de Jay Gatsby, un hombre rico y misterioso que organiza fiestas extravagantes en su mansión en Long Island. A través de los ojos del narrador, Nick Carraway, "El gran Gatsby" explora la corrupción y el vacío de la alta sociedad americana.',
+        imagen: '../public/images/products/gran-gatsby.webp',
+        fechaCarga:
+      },
+      {
+        nombre: 'El senior de los anillos',
+        descripción: 'Esta trilogía épica, escrita por J.R.R Tolkien, sigue las aventuras de un grupo de hobbits, enanos, elfos y humanos mientras buscan destruir el Anillo Único, un objeto poderoso que podría caer en manos equivocadas. "El señor de los anillos" es una obra maestra del género de la fantasía y ha sido adaptada al cine con gran éxito.',
+        imagen: '../public/images/products/senior-anillos.jpeg',
+        fechaCarga:
+      },
+      {
+        nombre: 'El aleph',
+        descripción: 'Esta colección de cuentos del escritor argentino Jorge Luis Borges explora temas como el tiempo, el espacio y la identidad. "El aleph" es una obra de gran complejidad y profundidad y es considerada una de las mejores colecciones de cuentos de la literatura en español.',
+        imagen: '../public/images/products/aleph.jpeg',
+        fechaCarga:
+      },
+      {
+        nombre: 'La sombra del viento',
+        descripción: 'Esta novela cuenta la historia de Daniel Sempere, un joven que descubre un libro en la librería de su padre y comienza a investigar sobre el autor, Julián Carax. A medida que Daniel profundiza en la vida de Carax, se adentra en una trama de amor, venganza y misterio en la Barcelona de posguerra.',
+        imagen: '../public/images/products/sombra-viento.jpeg',
+        fechaCarga:
+      },
+      {
+        nombre: 'La insoportable levedad del ser',
+        descripción: 'Ambientada en Praga en la década de 1960, esta novela explora la vida de cuatro personajes: Tomás, Teresa, Sabina y Franz. "La insoportable levedad del ser" es una reflexión sobre la libertad, el amor y la identidad, y es considerada una de las obras más importantes de la literatura europea contemporánea.',
+        imagen: '../public/images/products/levedad-ser.webp',
+        fechaCarga:
+      },
+      {
+        nombre: 'corte de rosas y espinas',
+        descripción: 'es una novela de fantasía escrita por la autora estadounidense Sarah J. Maas. La historia sigue a la protagonista, Feyre, una cazadora que mata a un lobo en el bosque, pero que es llevada al mundo mágico de los fae como castigo por su crimen. Allí, debe luchar por su supervivencia y descubrir la verdad detrás de los misteriosos sucesos que ocurren en el mundo de los fae. A medida que la trama se desarrolla, se revelan intrigas políticas, amores prohibidos y peligrosas amenazas que pondrán en peligro la vida de Feyre y de aquellos a quienes ama. "Corte de Rosas y Espinas" es el primer libro de una serie de novelas de fantasía épica y ha sido elogiado por su mundo detallado y sus personajes bien desarrollados. Es una obra que invita a la reflexión sobre el valor de la empatía y la lucha contra la injusticia en un mundo mágico lleno de peligros y sorpresas.',
+        imagen: '../public/images/products/rosas-espinas.jpeg',
+        fechaCarga:
+      },
+      {
+        nombre: 'La metamorfosis',
+        descripción: 'Este relato corto cuenta la historia de Gregor Samsa, un hombre que se despierta un día convertido en un insecto. "La metamorfosis" es una obra de la literatura moderna que explora la alienación, la soledad y la desesperación.',
+        imagen: '../public/images/products/metamorfosis.webp',
+        fechaCarga:
+      },
+      {
+        nombre: 'El principito',
+        descripción: 'Esta novela corta cuenta la historia de un niño que viaja de planeta en planeta y se encuentra con varios personajes que representan diferentes aspectos de la vida humana. "El principito" es una obra tierna y poética que ha sido traducida a más de 250 idiomas.',
+        imagen: '../public/images/products/principito.jpeg',
+        fechaCarga:
+      },
+      {
+        nombre: 'La casa de los espíritus',
+        descripción: 'Esta novela sigue la historia de la familia Trueba a lo largo de varias décadas en Chile. "La casa de los espíritus" es una obra que combina elementos del realismo mágico con la política y la historia de América Latina.',
+        imagen: '../public/images/products/casa-espiritus.jpeg',
+        fechaCarga:
+      }
+    ],
+    comentarios: [
+      {
+        nombreUsuario: 'miguelestrada24',
+        textoComentario: 'Este libro es excelente!',
+        imagenPerfil: '../public/images/users/default-image.png'
+      },
+      {
+        nombreUsuario: 'miguelestrada24',
+        textoComentario: 'Muy interesante!',
+        imagenPerfil: '../public/images/users/default-image.png'
+      },
+      // Agregar aquí más comentarios
+    ]
+  };
+  
