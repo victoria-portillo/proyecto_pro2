c5a130172285547a284a1e543a94236ea54eccc6
santospasman <spasman@udesa.edu.ar>
2023-06-15 16:46:42 -0300

    modelo productos

---
+++ catologo-productos/database/models/productos.js
@@ -0,0 +1,48 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = 'Productos';
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+        id_usuario: {
+            type: dataTypes.INTEGER,
+        },
+        nombre_producto: {
+            type: dataTypes.STRING(500),
+        },
+        descripcion_producto: {
+            type: dataTypes.STRING(500),
+        },
+        createdAt: {
+            type: dataTypes.date,
+        },
+        updatedAt: {
+            type: dataTypes.date,
+        },
+        deletedAt: {
+            type: dataTypes.date,
+        }
+    };
+    let config = {
+        tableName: 'Productos',
+        timestamps: true,
+        underscored: true,
+    };
+
+    const Producto = sequelize.define(alias,cols,config);
+    //relaciones
+
+    Producto.associate = function(models) {
+        // Un perfil --> muchos comentarios
+        Comentario.belongsToMany(models.Usuarios , {
+            as: "usuario",
+            through: "Usuario_producto",
+            foreignKey: "id_producto",
+            otherKey: "id_usuario"
+        })
+    };
+
+    return Comentario;
+};
